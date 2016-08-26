c228d11e71e83eee8bd6d4ae2e9bcb5f08bb18b4
Raphael Isemann <teemperor@gmail.com>
2016-08-26 08:52:47 +0200

    Fixed extra space in toolchain file

--- Toolchain-mingw32.cmake
+++ Toolchain-mingw32.cmake
@@ -8,12 +8,12 @@ SET(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-c++)
 SET(CMAKE_RC_COMPILER /usr/bin/i686-w64-mingw32-windres)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} /usr/i686-w64-mingw32/ ${CMAKE_CURRENT_SOURCE_DIR }/../winlib/ )
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} /usr/i686-w64-mingw32/ ${CMAKE_CURRENT_SOURCE_DIR}/../winlib/ )
 
 set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
 
-link_directories(${CMAKE_CURRENT_SOURCE_DIR }/../winlibs/libs/)
-include_directories(/${CMAKE_CURRENT_SOURCE_DIR }/../winlibs/include/)
+link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../winlibs/libs/)
+include_directories(/${CMAKE_CURRENT_SOURCE_DIR}/../winlibs/include/)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
